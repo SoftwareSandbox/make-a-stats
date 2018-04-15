4118b7fc80f3776e113828093a428c66eb521d6f
David Op de Beeck <davidopdebeeck@hotmail.com>
2018-04-15 22:15:41 +0200

    players are sorted by kills
    + updated styling of leaderboard and player component

--- frontend/src/components/LeaderBoard.vue
+++ frontend/src/components/LeaderBoard.vue
@@ -1,8 +1,8 @@
 <template>
-  <section>
+  <section class="leaderboard">
     <h1>LeaderBoard</h1>
     <section class="players">
-      <Player v-for="player in players" v-bind:key="player.playerId" v-bind:player="player"/>
+      <Player v-for="player in playersSortedByKills" v-bind:key="player.playerId" v-bind:player="player"/>
     </section>
   </section>
 </template>
@@ -20,6 +20,12 @@ export default {
       players: []
     }
   },
+  computed: {
+      playersSortedByKills: function () {
+        let players = this.players;
+        return players.sort((p1, p2) => p1.kills < p2.kills);
+      }
+  },
   created: function () {
     this.$http.get('api/stats/leaderboard')
       .then(response => this.players = response.body);
@@ -28,14 +34,26 @@ export default {
 </script>
 
 <style scoped>
+  .leaderboard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   h1 {
+    color: #303030;
     font-weight: 700;
-    font-size: 3rem;
+    font-size: 2.5rem;
     text-transform: uppercase;
     text-align: center;
   }
   .players {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    width: 400px;
+  }
+  @media screen and (max-width: 400px) {
+    .players {
+      width: 100%;
+    }
   }
 </style>
