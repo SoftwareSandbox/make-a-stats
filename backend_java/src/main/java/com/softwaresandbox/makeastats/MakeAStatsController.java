319a59b514e8905c732a004d043a7d2e2647d79e
Jooones <jonathanV@bitbucket.org>
2018-04-24 19:06:16 +0200

    Add cross origin annotation to support a different backend url from the ui

--- backend_java/src/main/java/com/softwaresandbox/makeastats/MakeAStatsController.java
+++ backend_java/src/main/java/com/softwaresandbox/makeastats/MakeAStatsController.java
@@ -2,14 +2,12 @@ package com.softwaresandbox.makeastats;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.http.ResponseEntity.ok;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/backend/")
 public class MakeAStatsController {
