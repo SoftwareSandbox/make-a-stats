e5989b5aa46dc18d1c273228fd88b0109053e550
sch3lp <sch3lp@gmail.com>
2018-04-24 21:37:10 +0200

    actually returning a 404 when no player found

--- pubg-stub/server.js
+++ pubg-stub/server.js
@@ -13,9 +13,7 @@ app.get('/pubg-stub/player', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     let playersWithName = players.data.filter((player) => playernames.indexOf(player.attributes.name.toLowerCase()) > -1);
     let result = Object.assign(players,{data:playersWithName});
-    console.log(`result = ${JSON.stringify(result)}`);
-    // playersWithName.length > 0 ? res.json(result) : res.status(404).render();
-    res.json(result);
+    playersWithName.length > 0 ? res.json(result) : res.sendStatus(404);
 });
 
 app.get('/pubg-stub/matches/:id', (req, res) => {
@@ -23,7 +21,7 @@ app.get('/pubg-stub/matches/:id', (req, res) => {
     console.log(`Returning match for id: ${id}`);
     res.set('Access-Control-Allow-Origin', '*');
     let matchesWithId = matches.filter((match) => match.data.id == id);
-    matchesWithId.length > 0 ? res.json(matchesWithId[0]) : res.status(404).render();
+    matchesWithId.length > 0 ? res.json(matchesWithId[0]) : res.sendStatus(404);
 });
 
 app.listen(3333, function() {
