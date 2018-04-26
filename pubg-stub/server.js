a96d06260aab3d02ebf6d97bdb08bb216f327350
David Op de Beeck <davidopdebeeck@hotmail.com>
2018-04-26 21:11:48 +0200

    renamed 'player' endpoint to 'players' and added camelCasing

--- pubg-stub/server.js
+++ pubg-stub/server.js
@@ -7,32 +7,32 @@ const {players} = require('./players');
 const {matches} = require('./matches');
 
 //?filter[playernames]=jooones
-app.get('/pubg-stub/player', (req, res) => {
-    let playernames = req.query.filter ? req.query.filter.playernames.split(',') : [];
-    if (isEmpty(playernames)) {
+app.get('/pubg-stub/players', (req, res) => {
+    let playerNames = req.query.filter ? req.query.filter.playerNames.split(',') : [];
+    if (isEmpty(playerNames)) {
         console.log(`Returning all players`);
     } else {
-        console.log(`Returning players ${playernames}`);
+        console.log(`Returning players ${playerNames}`);
     }
     res.set('Access-Control-Allow-Origin', '*');
     let playersWithName = players.data
-        .filter((player) => playernames.indexOf(player.attributes.name.toLowerCase()) > -1);
-    let result = isEmpty(playernames) ? players : {...players,...{data:playersWithName}};
-    (isEmpty(playernames) || playersWithName.length) > 0 ? res.json(result) : res.sendStatus(404);
+        .filter((player) => playerNames.indexOf(player.attributes.name) > -1);
+    let result = isEmpty(playerNames) ? players : {...players, ...{data: playersWithName}};
+    (isEmpty(playerNames) || playersWithName.length) > 0 ? res.json(result) : res.sendStatus(404);
 });
 
 app.get('/pubg-stub/matches/:id', (req, res) => {
     let id = req.params.id;
     console.log(`Returning match for id: ${id}`);
     res.set('Access-Control-Allow-Origin', '*');
-    let matchesWithId = matches.filter((match) => match.data.id == id);
+    let matchesWithId = matches.filter((match) => match.data.id === id);
     matchesWithId.length > 0 ? res.json(matchesWithId[0]) : res.sendStatus(404);
 });
 
-app.listen(3333, function() {
+app.listen(3333, function () {
     console.log('API listening on http://localhost:3333/pubg-stub')
 });
 
-function isEmpty(array){
+function isEmpty(array) {
     return _.isEmpty(array);
 }
