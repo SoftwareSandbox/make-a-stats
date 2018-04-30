566dbd5defd5662e105921da74abd9980e06a822
sch3lp <sch3lp@gmail.com>
2018-04-30 12:22:09 +0200

    mapping player matchid tuples

--- backend/src/PUBG/Call.elm
+++ backend/src/PUBG/Call.elm
@@ -7,45 +7,52 @@ import PUBG.API.Common exposing (..)
 import Task exposing (..)
 
 
-getPlayers : Http.Request (Wrapper Player)
+getPlayerMatches : Task String (List ( Player, List Match ))
+getPlayerMatches =
+    getPlayers
+        |> Task.map (unwrap >> matchIdsPerPlayers)
+        |> Task.andThen getMatchesPerPlayer
+
+
+getPlayers : Task String (Wrapper Player)
 getPlayers =
+    fetchPlayers
+        |> toHttpTask
+
+
+getMatchesPerPlayer : List ( Player, List MatchId ) -> Task String (List ( Player, List Match ))
+getMatchesPerPlayer matchIdsGroupedPerPlayer =
+    matchIdsGroupedPerPlayer
+        |> List.map getMatchesOfPlayer
+        |> Task.sequence
+
+
+getMatchesOfPlayer : ( Player, List MatchId ) -> Task String ( Player, List Match )
+getMatchesOfPlayer ( player, matchIds ) =
+    getMatches matchIds
+        |> Task.map (\ms -> ( player, ms ))
+
+
+getMatches : List MatchId -> Task String (List Match)
+getMatches matchIds =
+    List.map getMatch matchIds
+        |> Task.sequence
+
+
+getMatch : MatchId -> Task String Match
+getMatch matchId =
+    fetchMatch matchId
+        |> toHttpTask
+
+
+fetchPlayers : Http.Request (Wrapper Player)
+fetchPlayers =
     Http.get ("http://localhost:3333/pubg-stub/players") (wrap playerDecoder)
 
 
-getMatch : MatchId -> Http.Request Match
-getMatch matchId =
-    Http.get ("http://localhost:3333/pubg-stub/matches/" ++ matchId) matchDecoder
-
-
-fetchPlayers : Task String (Wrapper Player)
-fetchPlayers =
-    getPlayers
-        |> toHttpTask
-
-
-fetchMatch : MatchId -> Task String Match
+fetchMatch : MatchId -> Http.Request Match
 fetchMatch matchId =
-    getMatch matchId
-        |> toHttpTask
-
-
-fetchMatches : List MatchId -> Task String (List Match)
-fetchMatches matchIds =
-    List.map fetchMatch matchIds
-        |> Task.sequence
-
-
-fetchMatchesPerPlayer : List ( Player, List MatchId ) -> Task String (List ( Player, List Match ))
-fetchMatchesPerPlayer matchIdsGroupedPerPlayer =
-    List.map (\( p, ms ) -> ( p, fetchMatch ms )) matchIdsGroupedPerPlayer
-        |> Task.sequence
-
-
-fetchPlayerMatches : Task String (List ( Player, List Match ))
-fetchPlayerMatches =
-    fetchPlayers
-        |> Task.map (unwrap >> matchIdsPerPlayers)
-        |> Task.andThen fetchMatchesPerPlayer
+    Http.get ("http://localhost:3333/pubg-stub/matches/" ++ matchId) matchDecoder
 
 
 
