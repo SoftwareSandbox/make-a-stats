9d8b644aa255db0258ce9dff2f86445e1abdbea8
sch3lp <sch3lp@gmail.com>
2018-06-01 21:24:02 +0200

    Using playername type alias

--- backend_kotlin/src/main/kotlin/be/swsb/makeastats/kotlinbackend/model/PlayerStats.kt
+++ backend_kotlin/src/main/kotlin/be/swsb/makeastats/kotlinbackend/model/PlayerStats.kt
@@ -3,12 +3,12 @@ package be.swsb.makeastats.kotlinbackend.model
 import java.util.*
 
 data class PlayerStats(val id: PlayerStatsId,
-                       val player: String,
+                       val player: PlayerName,
                        val totalKills: Int? = 0,
                        val amountOfMatchesPlayed: Int? = 0,
                        val killsPerMatch: Float? = 0f) {
     companion object {
-        fun fromPlayernames(names: Iterable<String>): List<PlayerStats> =
+        fun fromPlayernames(names: Iterable<PlayerName>): List<PlayerStats> =
                 names.map { name -> PlayerStats(UUID.randomUUID(), name) }
     }
 }
