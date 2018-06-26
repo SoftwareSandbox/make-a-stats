c89aa4e6e73fb230751c337ebd625ec03b2418cf
sch3lp <sch3lp@gmail.com>
2018-06-26 21:42:21 +0200

    Replaced nullable props with lateinit in some tests

--- backend_kotlin/application/src/test/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/LeaderboardRepoTest.kt
+++ backend_kotlin/application/src/test/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/LeaderboardRepoTest.kt
@@ -1,6 +1,7 @@
 package be.swsb.makeastats.kotlinbackend.domain.leaderboard
 
 import be.swsb.makeastats.kotlinbackend.domain.leaderboard.LeaderboardTestBuilder.Companion.aLeaderboard
+import be.swsb.makeastats.kotlinbackend.domain.leaderboard.LeaderboardTestBuilder.Companion.aLeaderboardWithoutPlayers
 import be.swsb.makeastats.kotlinbackend.test.JdbiPreparedEmbeddedPostgresKotlin
 import org.assertj.core.api.Assertions
 import org.jdbi.v3.sqlobject.kotlin.onDemand
@@ -15,7 +16,7 @@ class LeaderboardRepoTest {
     @Rule @JvmField
     val db: JdbiRule = JdbiPreparedEmbeddedPostgresKotlin.preparedJdbi("db/migration").withPlugins();
 
-    private var repo: LeaderboardRepo? = null
+    private lateinit var repo: LeaderboardRepo
 
     @Before
     fun setUp() {
@@ -23,27 +24,27 @@ class LeaderboardRepoTest {
     }
 
     @Test
-    fun canSaveLeaderboard() {
-        val leaderboard = aLeaderboard(UUID.randomUUID(), "UvOiox", "ZF")
-        val savedLeaderboard = repo?.insertAndFind(leaderboard)
+    fun canSaveLeaderboard_WithoutPlayers() {
+        val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), "UvOiox", "ZF")
+        val savedLeaderboard = repo.insertAndFind(leaderboard)
         Assertions.assertThat(savedLeaderboard).isEqualTo(leaderboard)
     }
 
     @Test
     fun canFindLeaderboardByLeaderboardId() {
         val lid = "UvOiox"
-        val leaderboard = aLeaderboard(UUID.randomUUID(), lid, "ZF")
-        repo?.insert(leaderboard)
-        val savedLeaderboard = repo?.findByLeaderboardId(lid)
+        val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), lid, "ZF")
+        repo.insert(leaderboard)
+        val savedLeaderboard = repo.findByLeaderboardId(lid)
         Assertions.assertThat(savedLeaderboard).isEqualTo(leaderboard)
     }
 
     @Test
     fun cannotFindLeaderboardByLeaderboardId_ReturnsNull() {
         val lid = "UvOiox"
-        val leaderboard = aLeaderboard(UUID.randomUUID(), lid, "ZF")
-        repo?.insert(leaderboard)
-        val actual = repo?.findByLeaderboardId("snarfsnarf")
+        val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), lid, "ZF")
+        repo.insert(leaderboard)
+        val actual = repo.findByLeaderboardId("snarfsnarf")
         Assertions.assertThat(actual).isNull()
     }
 }
