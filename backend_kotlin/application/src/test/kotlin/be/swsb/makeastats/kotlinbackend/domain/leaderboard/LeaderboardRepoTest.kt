ca04f4140da56b8a9b7494ce540e4388cc676258
sch3lp <sch3lp@gmail.com>
2018-07-08 17:49:58 +0200

    LeaderboardService can save and return a LeaderboardWithPlayers

--- backend_kotlin/application/src/test/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/LeaderboardRepoTest.kt
+++ backend_kotlin/application/src/test/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/LeaderboardRepoTest.kt
@@ -2,6 +2,8 @@ package be.swsb.makeastats.kotlinbackend.domain.leaderboard
 
 import be.swsb.makeastats.kotlinbackend.domain.leaderboard.LeaderboardTestBuilder.Companion.aLeaderboard
 import be.swsb.makeastats.kotlinbackend.domain.leaderboard.LeaderboardTestBuilder.Companion.aLeaderboardWithoutPlayers
+import be.swsb.makeastats.kotlinbackend.domain.playerstats.PlayerStats
+import be.swsb.makeastats.kotlinbackend.domain.playerstats.PlayerStatsRepo
 import be.swsb.makeastats.kotlinbackend.test.JdbiPreparedEmbeddedPostgresKotlin
 import org.assertj.core.api.Assertions
 import org.jdbi.v3.sqlobject.kotlin.onDemand
@@ -16,26 +18,62 @@ class LeaderboardRepoTest {
     @Rule @JvmField
     val db: JdbiRule = JdbiPreparedEmbeddedPostgresKotlin.preparedJdbi("db/migration").withPlugins();
 
-    private lateinit var repo: LeaderboardRepo
+    private lateinit var leaderboardRepo: LeaderboardRepo
+    private lateinit var playerStatsRepo: PlayerStatsRepo
 
     @Before
     fun setUp() {
-        repo = db.jdbi.onDemand()
+        leaderboardRepo = db.jdbi.onDemand()
+        playerStatsRepo = db.jdbi.onDemand()
     }
 
     @Test
     fun canSaveLeaderboard_WithoutPlayers() {
         val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), "UvOiox", "ZF")
-        val savedLeaderboard = repo.insertAndFind(leaderboard)
+        val savedLeaderboard = leaderboardRepo.insertAndFind(leaderboard)
+
         Assertions.assertThat(savedLeaderboard).isEqualTo(leaderboard)
+    }
+
+    @Test
+    fun canAddPlayersToLeaderboard() {
+        val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), "UvOiox", "ZF")
+        val savedLeaderboard = leaderboardRepo.insertAndFind(leaderboard)
+
+        val playerId1 = createAndPersistPlayerStats("womble").id
+        val playerId2 = createAndPersistPlayerStats("cyanide").id
+
+        leaderboardRepo.addPlayersToLeaderboard(savedLeaderboard.lid, listOf(playerId1, playerId2))
+    }
+
+    @Test
+    fun canFindLeaderboardWithPlayers_LeaderboardWithGivenLeaderboardIdExists() {
+        val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), "UvOiox", "ZF")
+        val savedLeaderboard = leaderboardRepo.insertAndFind(leaderboard)
+
+        val player1 = createAndPersistPlayerStats("womble")
+        val player2 = createAndPersistPlayerStats("cyanide")
+
+        leaderboardRepo.addPlayersToLeaderboard(savedLeaderboard.lid, listOf(player1.id, player2.id))
+        val leaderboardWithPlayers = leaderboardRepo.findLeaderboardWithPlayersByLeaderboardId(savedLeaderboard.lid)
+
+        Assertions.assertThat(leaderboardWithPlayers).containsOnly(LeaderboardRepo.LeaderboardPlayer(leaderboard.lid, player1.id), LeaderboardRepo.LeaderboardPlayer(leaderboard.lid, player2.id))
+    }
+
+    @Test
+    fun cannotFindLeaderboardWithPlayers_LeaderboardWithGivenLeaderboardIdDoesNotExist() {
+        val actual = leaderboardRepo.findLeaderboardWithPlayersByLeaderboardId("snarfsnarf")
+
+        Assertions.assertThat(actual).isEmpty()
     }
 
     @Test
     fun canFindLeaderboardByLeaderboardId() {
         val lid = "UvOiox"
         val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), lid, "ZF")
-        repo.insert(leaderboard)
-        val savedLeaderboard = repo.findByLeaderboardId(lid)
+        leaderboardRepo.insert(leaderboard)
+        val savedLeaderboard = leaderboardRepo.findByLeaderboardId(lid)
+
         Assertions.assertThat(savedLeaderboard).isEqualTo(leaderboard)
     }
 
@@ -43,8 +81,16 @@ class LeaderboardRepoTest {
     fun cannotFindLeaderboardByLeaderboardId_ReturnsNull() {
         val lid = "UvOiox"
         val leaderboard = aLeaderboardWithoutPlayers(UUID.randomUUID(), lid, "ZF")
-        repo.insert(leaderboard)
-        val actual = repo.findByLeaderboardId("snarfsnarf")
+        leaderboardRepo.insert(leaderboard)
+        val actual = leaderboardRepo.findByLeaderboardId("snarfsnarf")
+
         Assertions.assertThat(actual).isNull()
+    }
+
+    private fun createAndPersistPlayerStats(name: String): PlayerStats {
+        val id = UUID.randomUUID()
+        val player = PlayerStats(id, name, 12, 1, 12f)
+
+        return playerStatsRepo.insertAndFind(player)
     }
 }
--- backend_kotlin/application/src/test/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/LeaderboardTestBuilder.kt
+++ backend_kotlin/application/src/test/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/LeaderboardTestBuilder.kt
@@ -1,30 +1,24 @@
 package be.swsb.makeastats.kotlinbackend.domain.leaderboard
 
-import be.swsb.makeastats.kotlinbackend.domain.playerstats.PlayerStatsId
 import java.util.*
 
 class LeaderboardTestBuilder private constructor() {
     companion object {
         fun aLeaderboardWithoutPlayers(id:UUID, lid: LeaderboardHashId, name: String): Leaderboard {
-            return Leaderboard(id, lid, name, null)
+            return Leaderboard(id, lid, name)
         }
 
         fun aLeaderboard(): LeaderboardTestBuilder {
             return LeaderboardTestBuilder()
         }
-//        TODO
-//        fun create(id: UUID = UUID.randomUUID(), lid: LeaderboardHashId = "123456", name: String = "LeaderboardName"): Leaderboard {
-//            return Leaderboard(id, lid, name, emptyList())
-//        }
     }
 
     private var id: UUID? = null
     private var lid: LeaderboardHashId? = null
     private var name: String? = null
-    private var players: List<PlayerStatsId>? = null
 
     fun build(): Leaderboard {
-        return Leaderboard(id!!, lid!!, name!!, players)
+        return Leaderboard(id!!, lid!!, name!!)
     }
 
     fun withId(id: UUID): LeaderboardTestBuilder {
@@ -39,11 +33,6 @@ class LeaderboardTestBuilder private constructor() {
 
     fun withName(name: String): LeaderboardTestBuilder {
         this.name = name
-        return this
-    }
-
-    fun withPlayers(players: List<PlayerStatsId>): LeaderboardTestBuilder {
-        this.players = players
         return this
     }
 }
