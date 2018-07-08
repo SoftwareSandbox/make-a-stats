b2d11f8df8feded5aac1965cadaa9b7cd8141473
sch3lp <sch3lp@gmail.com>
2018-07-08 17:49:58 +0200

    LeaderboardService can save and return a LeaderboardWithPlayers

--- backend_kotlin/application/src/main/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/Leaderboard.kt
+++ backend_kotlin/application/src/main/kotlin/be/swsb/makeastats/kotlinbackend/domain/leaderboard/Leaderboard.kt
@@ -1,6 +1,6 @@
 package be.swsb.makeastats.kotlinbackend.domain.leaderboard
 
-import be.swsb.makeastats.kotlinbackend.domain.playerstats.PlayerStatsId
+import be.swsb.makeastats.kotlinbackend.domain.playerstats.PlayerStats
 import be.swsb.makeastats.kotlinbackend.util.hashids.Hashids
 import java.util.*
 
@@ -8,13 +8,11 @@ import java.util.*
  * @param id: database id; do not expose
  * @param lid: exposed hashId (to use in url's)
  * @param name: name of the leaderboard
- * @param players: list of players that belong to this leaderboard
  */
 data class Leaderboard constructor(val id: UUID,
-                                            val lid: LeaderboardHashId,
-                                            val name: String,
-                                            val players: List<PlayerStatsId>?) {
-    constructor(cmd: CreateLeaderBoardCmd) : this(UUID.randomUUID(), generateUniqueId(), cmd.name, null)
+                                   val lid: LeaderboardHashId,
+                                   val name: String) {
+    constructor(cmd: CreateLeaderBoardCmd) : this(UUID.randomUUID(), generateUniqueId(), cmd.name)
 
     companion object {
         fun generateUniqueId(): LeaderboardHashId {
@@ -25,3 +23,14 @@ data class Leaderboard constructor(val id: UUID,
 }
 
 typealias LeaderboardHashId = String
+
+/**
+ * @param id: database id; do not expose
+ * @param lid: exposed hashId (to use in url's)
+ * @param name: name of the leaderboard
+ * @param players: list of players and their stats that belong to this leaderboard
+ */
+data class LeaderboardWithPlayers constructor(val id: UUID,
+                                              val lid: LeaderboardHashId,
+                                              val name: String,
+                                              val players: List<PlayerStats?>)
